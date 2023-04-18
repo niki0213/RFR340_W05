f922d303359e7500287ed88dfedfb8e05361c331
niki.nagy@stud.uni-obuda.hu <niki.nagy@stud.uni-obuda.hu>
2023-04-18 14:22:49 +0200

    Controller created

---
+++ Backend/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using Logic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace Endpoint.Controllers
+{
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        IMessageLogic logic;
+        public MessageController(IMessageLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Message value)
+        {
+            this.logic.Add(value);
+        }
+
+    }
+}
